a9a3da777ddad9310791fe2c2e9dae67980109aa
Carolina <carolina.kaji@gmail.com>
2021-04-09 20:50:53 -0300

    Criado estrutura inicial

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+const PATH_ROOT = __DIR__;
+const URL = "http://localhost/carolkaji/ds1/p1/comentarios";
+// new PDO('mysql:host=localhost;port=3306;dbname=cadastro', 'root', '');
+const db = [
+  'host' => 'mysql:host=localhost;port=3306;dbname=cadastro',
+  'user' => 'root',
+  'pass' => '',
+  'dbname' => 'cadastro'
+];
