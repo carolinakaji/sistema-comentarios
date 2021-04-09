70e08d6e70cde2225d2a7c6ad98884551e682006
Carolina <carolina.kaji@gmail.com>
2021-04-09 20:56:54 -0300

    Alterado localização da minha URL

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 const PATH_ROOT = __DIR__;
-const URL = "http://localhost/carolkaji/ds1/p1/comentarios";
+const URL = "http://localhost/ds1/prova2";
 // new PDO('mysql:host=localhost;port=3306;dbname=cadastro', 'root', '');
 const db = [
   'host' => 'mysql:host=localhost;port=3306;dbname=cadastro',
