fbe7368b0043328ec2deb67721b98e3792c68c7b
Carolina <carolina.kaji@gmail.com>
2021-04-10 11:29:26 -0300

    chamada para cadastrar novo usuário

--- pages/cadastrarUsuario.php
+++ pages/cadastrarUsuario.php
@@ -2,6 +2,12 @@
 
 include __DIR__ . '/../config.php';
 
+$senha = isset($_POST['senha']) ?? $_POST['senha'];
+if (isset($_POST['cadastrar'])) {
+  $senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+  postUsuario($nome, $email, $cidade, $estado, $senhaSegura, $imagem);
+}
+
 
 include PATH_ROOT . "/includes/header.php";
 include PATH_ROOT . "/includes/formCadastroUsuario.php";
