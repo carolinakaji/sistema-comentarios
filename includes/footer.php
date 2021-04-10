472a81b29f9824f12159c35cfd6dee37ab309b14
Carolina <carolina.kaji@gmail.com>
2021-04-10 13:35:25 -0300

    Ajusta no footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="row py-3 footer-bg text-white fixed-bottom">
   <div class="container text-center">
-    <small>Copyright &copy; | <a href="https://github.com/carolinakaji" target="_blank"> | Fatec 4ºSemestre 2021</small>
+    <small>Copyright &copy; | <a href="https://github.com/carolinakaji" target="_blank"> Carol Kaji</a> | Fatec 4ºSemestre 2021</small>
   </div>
 </footer>
 </body>
