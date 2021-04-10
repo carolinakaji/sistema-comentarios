d69d4b8e57470282ee3b691c969476b611b0e02e
Carolina <carolina.kaji@gmail.com>
2021-04-10 13:39:29 -0300

    Adicionando container, atributo name no textarea

--- includes/formComentarios.php
+++ includes/formComentarios.php
@@ -1,5 +1,7 @@
-<div class="form-group ">
-  <label for="exampleFormControlTextarea1">Deixe seu comentário</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  <button type='submit' class="btn btn-green mb-2" name="publicar">Publicar</button>
+<div class='container'>
+  <div class="form-group ">
+    <label for="exampleFormControlTextarea1">Deixe seu comentário</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="comentario"></textarea>
+    <button type='submit' class="btn btn-green mb-2" name="publicar">Publicar</button>
+  </div>
 </div>
