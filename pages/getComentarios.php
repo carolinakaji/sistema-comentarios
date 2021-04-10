557845eb874cd15d72b71a34aeb315eda1434744
Carolina <carolina.kaji@gmail.com>
2021-04-10 13:44:04 -0300

    Ajuste no layout

--- pages/getComentarios.php
+++ pages/getComentarios.php
@@ -2,7 +2,6 @@
 
 include __DIR__ . '/../config.php';
 
-
 include PATH_ROOT . "/includes/header.php";
 include PATH_ROOT . "/includes/formComentarios.php";
 include PATH_ROOT . "/includes/footer.php";
