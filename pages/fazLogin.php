a950139fcc502985e9191f5768ccbe54b0ea4635
Carolina <carolina.kaji@gmail.com>
2021-04-12 08:32:25 -0300

    Para fazer o loginAtivo.php funcionar corretamente e ser chamado, é necessário adicionar o diretorio config e o path do bancoDados.php

--- pages/fazLogin.php
+++ pages/fazLogin.php
@@ -1,10 +1,11 @@
 <?php
 include_once __DIR__ . "/../config.php";
-include PATH_ROOT . "/db/bancoDados.php";
-
+//include PATH_ROOT . "/db/bancoDados.php";
+$msgAlerta = '';
 if (isset($_POST['login'])) {
   if ($_POST['email'] == '' && $_POST['senha'] == '') {
     $msgAlerta = 'Os campos devem ser preenchidos';
+  } else {
+    login($_POST['email'], $_POST['senha']);
   }
-  login($_POST['email'], $_POST['senha']);
 }
