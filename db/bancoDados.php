b7350a7ba92d8a83160a310b935644e5f0acf82e
Carolina <carolina.kaji@gmail.com>
2021-04-14 01:27:24 -0300

    Post de comentários para logados e anonimos

--- db/bancoDados.php
+++ db/bancoDados.php
@@ -1,7 +1,6 @@
 <?php
 include_once __DIR__ . "/../config.php";
 
-
 function abrirConnection()
 {
   try {
@@ -12,9 +11,13 @@ function abrirConnection()
     exit;
   }
 }
+function fecharConexao()
+{
+  $connection = NULL;
+  return $connection;
+}
 
 // CADASTRO USUÁRIO
-
 function postUsuario($nome, $email, $cidade, $estado, $senhaSegura, $imagem)
 {
   $sql = "insert into usuarios (nome, email, cidade, estado, senha, imagem) values (:nome, :email, :cidade, :estado, :senha, :imagem)";
@@ -26,12 +29,12 @@ function postUsuario($nome, $email, $cidade, $estado, $senhaSegura, $imagem)
   $result->bindValue(':senha', $senhaSegura);
   $result->bindValue(':imagem', $imagem);
   $result->execute();
-  //fecharConexao();
+  fecharConexao();
 
   $alertaMensagem = "Cadastro realizado com sucesso";
 }
 
-// 
+
 // LOGAR
 function login($email, $senha)
 {
@@ -42,22 +45,121 @@ function login($email, $senha)
   $usuarios = $result->fetchAll(PDO::FETCH_ASSOC);
   foreach ($usuarios as $usuario) {
     if (password_verify($senha, $usuario['senha']) && $usuario['email'] === $email) {
+      
       $_SESSION['email'] = $usuario['email'];
-      header("location: ../index.php");
+      $_SESSION['id'] = $usuario['id'];
+      echo 'Quem logado: ' . $_SESSION['id'];
+      // header("location: ../index.php");
+
     }
   }
 }
 
-// GET COMENTÁRIOS
-/*function getComentarios()
+/****************** COMENTÁRIOS ******************/
+
+// POST COMENTARIO
+function postComentario($comentario, $idUsuarioLogado)
 {
-  $sql = "select * from comentarios";
+  
+  $currentDateTime = dataBrasil(date('Y/d/m H:i'));
+  $sql = "insert into comentarios (comentario, date, usuario) values (:comentario, :date, :usuario)";
+  $result = abrirConnection()->prepare($sql);
+  $result->bindValue(':comentario', $comentario);
+  $result->bindValue(':date', $currentDateTime);
+  $result->bindValue(':usuario', $idUsuarioLogado);
+
+  $result->execute();
+  fecharConexao();
+
+  $alertaMensagem = "Cadastro realizado com sucesso";
+}
+
+// GET COMENTÁRIOS
+function getComentarios()
+{
+  $comments = '';
+  $sql = "select * from usuarios 
+          right join comentarios 
+          on usuarios.id = comentarios.usuario";
   $result = abrirConnection()->prepare($sql);
   $result->execute();
   $usuarios = $result->fetchAll(PDO::FETCH_ASSOC);
-  foreach($comentarios as )
+
+  foreach($usuarios as $usuario){
+      if($usuario['usuario'] == null){
+        $usuario['nome'] = 'Anônimo';
+        $usuario['imagem'] = 'anonim.jpg';
+      }
+      $dataEHora = dataBrasil($usuario['date']);
+      $comments .="
+      <div class='row'>
+        <div class='col-2'>
+          <img src='../src/imgs/{$usuario['imagem']}' width='80' height='80'>
+        </div>
+        <div class='col-10'>
+          <div class='d-inline-flex'>
+            <h5>{$usuario['nome']} - {$dataEHora}</h5>
+          </div>
+          <p>{$usuario['comentario']}</p>
+        </div>
+      </div>
+      <hr>";
+    }
+  return $comments;
 }
-*/
+
+// GET COMENTÁRIOS POR ID
+// function getComentariosEmail($id)
+// {
+//   $editarBtn = verificaQuemEstaLogado();
+//   $comments = '';
+//   $sql = "select * from usuarios 
+//           inner join comentarios 
+//           on usuarios.id = comentarios.usuario where usuario.id=:id";
+//   $result = abrirConnection()->prepare($sql);
+//   $result->bindValue(':id', $id);
+//   $result->execute();
+//   $usuarios = $result->fetchAll(PDO::FETCH_ASSOC);
+//   foreach($usuarios as $usuario){
+//     $dataEHora = dataBrasil($usuario['date']);
+//     $comments .="
+//                   <div>
+//                     <div class='d-inline-flex'>
+//                       <h5>{$usuario['nome']} - {$dataEHora}</h5> {$editarBtn}<button class='btn ml-4'> asdfasdf</button>
+//                     </div>
+//                     <p>{$usuario['comentario']}</p>
+//                   </div>";
+//   }
+//   return $comments;
+// }
+
+// UPDATE COMENTÁRIOS
+function update($id,$comentario)
+{
+  $sql = "update comentarios set (comentario=:comentario) where id=:id";
+  $var = abrirConexao();
+  $result = $var->prepare($sql);
+  $result->bindValue(':comentario', $comentario);
+  $result->bindValue(':id', $id);
+  $result->execute();
+  fecharConexao();
+}
+
+function dataBrasil($data){
+  return date('d/m/Y H:m', strtotime($data));
+}
+
+function verificaQuemEstaLogado($email){
+  $sql = "select id from usuarios where email=:email";
+  $result = abrirConnection()->prepare($sql);
+  $result->bindValue(':email', $email);
+  $result->execute();
+  $usuarios = $result->fetchAll(PDO::FETCH_ASSOC);
+  };
+  
+
+
+
 /*
 function delete($id)
 {
@@ -66,27 +168,5 @@ function delete($id)
   $result->bindValue(':nome', $id);
   $result->execute();
   fecharConexao();
-}
-
-function update($nome, $email)
-{
-  $sql = "update clientes set (cidade= :cidade, estado = :estado, ) where email= :email";
-  $var = abrirConexao();
-  $result = $var->prepare($sql);
-  $result->bindValue(':estado', $estado);
-  $result->bindValue(':cidade', $cidade);
-  $result->bindValue(':email', $email);
-  $result->bindValue(':email', $email);
-  $result->execute();
-  fecharConexao();
-}
-*/
-/** TODO
- * Continuar update.
- * 
- *//*
-function getComentarios()
-{
-  $sql="select * from clientes";
 }
 */
---
+++ pages/cadastraComentario.php
@@ -0,0 +1,14 @@
+<?php
+include_once __DIR__ . "/../config.php";
+
+$msgAlertaComentario = '';
+$_SESSION['id']=false;
+if (isset($_POST['publicar'])) {
+  if ($_POST['comentario'] == '') {
+    $msgAlertaComentario = "O campo deve conter texto.";
+  } else if(!$_SESSION['id']){
+    postComentario($_POST['comentario'], null);
+  } else {
+    postComentario($_POST['comentario'], $_SESSION['id']);
+  }
+}
